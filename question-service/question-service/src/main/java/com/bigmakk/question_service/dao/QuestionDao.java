d1ddcb2af4c1a34a57703653953d7e1dfe6ef02e
Kashish Makkar <kashishmakkar99@gmail.com>
2025-02-13 02:14:12 +0530

    question service

---
+++ question-service/question-service/src/main/java/com/bigmakk/question_service/dao/QuestionDao.java
@@ -0,0 +1,21 @@
+package com.bigmakk.question_service.dao;
+
+
+
+import com.bigmakk.question_service.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question,Integer> {
+
+    List<Question> findByCategory(String category);
+
+    @Query(value = "SELECT * FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :num",nativeQuery = true)
+    List<Question> findRandomQuestionsByCategory(String category, int num);
+
+
+}
---
+++ question-service/question-service/src/main/java/com/bigmakk/question_service/service/QuestionService.java
@@ -0,0 +1,55 @@
+package com.bigmakk.question_service.service;
+
+
+import com.bigmakk.question_service.dao.QuestionDao;
+import com.bigmakk.question_service.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        try{
+            List<Question>qlist=new ArrayList<>();
+            qlist=questionDao.findAll();
+            return new ResponseEntity<>(qlist, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+
+        try{
+            List<Question>qlist=new ArrayList<>();
+            qlist=questionDao.findByCategory(category);;
+            return new ResponseEntity<>(qlist, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        try{
+          questionDao.save(question);
+          return new ResponseEntity<>("Success",HttpStatus.CREATED);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>("failure",HttpStatus.BAD_REQUEST);
+    }
+}
